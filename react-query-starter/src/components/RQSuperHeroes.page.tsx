cafc36d47186fc744cac614d39cd3b397c2876ea
이지호 <cctv1998@naver.com>
2022-01-27 21:57:43 +0900

    Feat : cacheTime,staleTime

--- react-query-starter/src/components/RQSuperHeroes.page.tsx
+++ react-query-starter/src/components/RQSuperHeroes.page.tsx
@@ -8,13 +8,13 @@ const fetchSuperHeros = async () => {
 };
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, error } = useQuery<Hero[], Error>(
+  const { isLoading, data, error, isFetching } = useQuery<Hero[], Error>(
     "super-heros",
     fetchSuperHeros,
-    { cacheTime: 5 * 60 * 1000 }
+    { staleTime: 5 * 60 * 1000 }
   );
 
-  console.log(data);
+  console.log("isLoading : ", isLoading, "isFetching : ", isFetching);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
@@ -30,3 +30,11 @@ export const RQSuperHeroesPage = () => {
     </>
   );
 };
+
+// staleTime :
+// 더 길게 지정하면 staleTime쿼리가 데이터를 자주 다시 가져오지 않습니다.
+// 기본값 : 0초 이유 : 0초가 안전해서
+
+// cacheTime :
+// 기본적으로 "비활성" 쿼리는 5분 후에 가비지 수집 됩니다.
+// 이를 변경하려면 cacheTime쿼리 의 기본값 을 1000 60 5밀리초가 아닌 다른 값 으로 변경할 수 있습니다 .
