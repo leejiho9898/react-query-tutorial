ebebcbac43343a49b421ad23d6419a52826da5a5
이지호 <cctv1998@naver.com>
2022-01-27 21:39:28 +0900

    Feat : react query 적용

--- react-query-starter/src/components/RQSuperHeroes.page.tsx
+++ react-query-starter/src/components/RQSuperHeroes.page.tsx
@@ -1,3 +1,32 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+import { Hero } from "type/type";
+
+const fetchSuperHeros = async () => {
+  const response = await axios.get("http://localhost:4000/superheroes");
+  return response.data;
+};
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
-}
+  const { isLoading, data, error } = useQuery<Hero[], Error>(
+    "super-heros",
+    fetchSuperHeros,
+    { cacheTime: 5 * 60 * 1000 }
+  );
+
+  console.log(data);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>Error : {error.message}</h2>;
+  }
+  return (
+    <>
+      <h2>Super Heroes Page</h2>
+      {data && data.map((hero) => <div key={hero.name}>{hero.name}</div>)}
+    </>
+  );
+};
