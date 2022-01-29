2e9b5cab2480f4df3ea3011de99ab8f2c76bdf90
이지호 <cctv1998@naver.com>
2022-01-29 19:30:17 +0900

    Comment : refetchOnMount,refetchInterval 주석

--- react-query-starter/src/components/RQSuperHeroes.page.tsx
+++ react-query-starter/src/components/RQSuperHeroes.page.tsx
@@ -1,17 +1,20 @@
 import axios from "axios";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import { Hero } from "type/type";
 
 const fetchSuperHeros = async () => {
   const response = await axios.get("http://localhost:4000/superheroes");
+  // const response = await axios.get("http://localhost:4000/superheroes?pageNo=1");
   return response.data;
 };
 
 export const RQSuperHeroesPage = () => {
+  const [count, setCount] = useState(0);
   const { isLoading, data, error, isFetching } = useQuery<Hero[], Error>(
-    "super-heros",
+    ["super-heros", count, "가격높은순"],
     fetchSuperHeros,
-    { staleTime: 5 * 60 * 1000 }
+    { staleTime: 20000 }
   );
 
   console.log("isLoading : ", isLoading, "isFetching : ", isFetching);
@@ -27,6 +30,8 @@ export const RQSuperHeroesPage = () => {
     <>
       <h2>Super Heroes Page</h2>
       {data && data.map((hero) => <div key={hero.name}>{hero.name}</div>)}
+      <button onClick={() => setCount(count + 1)}>상태+</button>
+      <button onClick={() => setCount(count - 1)}>상태-</button>
     </>
   );
 };
@@ -38,3 +43,8 @@ export const RQSuperHeroesPage = () => {
 // cacheTime :
 // 기본적으로 "비활성" 쿼리는 5분 후에 가비지 수집 됩니다.
 // 이를 변경하려면 cacheTime쿼리 의 기본값 을 1000 60 5밀리초가 아닌 다른 값 으로 변경할 수 있습니다 .
+
+//refetchOnMount : mount 될때마다 매번 fetch (true,false."always")
+
+//refetchInterval : 실시간 데이터 fetch나 주식같이 초단위로 fetch 해야하는 경우 사용
+//refetchIntervalBackground : 백그라운드에서도 초단위로 refetch
